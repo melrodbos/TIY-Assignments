c5af9eeede1189b48fc4cc84a693a758bef4f85b
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-07 16:58:01 -0400

    Class practice

---
+++ string-calculator.js
@@ -0,0 +1,37 @@
+var expect = require('chai').expect;
+
+function toEnglish(word){
+  if( word === "zero" )
+    return 0;
+  }
+
+  if( word === "one" ){
+    return 1;
+  }
+
+  if ( word === "two" ){
+    return 2;
+  }
+
+  if ( word === "three"){
+    return 3;
+  }
+
+  if ( word === "four"){
+    return 4;
+  }
+
+  if ( word === "five"){
+    return 5;
+    
+  }
+
+expect(toEnglish("zero")).to.equal(0);
+
+expect(toEnglish("one")).to.equal(1);
+
+expect(toEnglish("two")).to.equal(2);
+
+expect(toEnglish("three")).to.equal(3);
+
+expect(toEnglish("four")).to.equal(4);
