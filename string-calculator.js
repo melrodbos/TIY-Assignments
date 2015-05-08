30db5060712b9664cdb5cdaffbd5e4a0eb925060
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-08 08:37:53 -0400

    Fail test

--- string-calculator.js
+++ string-calculator.js
@@ -4,6 +4,11 @@ function toNumber(word){
   if ( word === "zero" ){
     return 0;
   }
+
+  if ( word === "zero"){
+    return 1;
+  }
 }
 
   expect(toNumber("zero")).to.equal(0);
+  expect(toNumber("zero")).to.equal(2);
