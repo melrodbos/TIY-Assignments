fb8d685d3fb2f9179ce6a3435758829d1686a752
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-08 11:47:27 -0400

    addition test

---
+++ practice-string-calculator.js
@@ -0,0 +1,22 @@
+var expect = require("chai").expect;
+
+function toNumbers(A, B){
+  return 0 + 0;
+}
+
+expect(toNumbers).to.exist;
+expect(toNumbers("zero", "zero")).to.equal(0);
+
+
+
+
+
+
+
+
+  /*function toNumber( A + B ){
+    return 0 + 1;
+  }
+
+  expect(plus).to.exist;
+  expect(toNumber(0 + 1)).to.equal(1)*/
