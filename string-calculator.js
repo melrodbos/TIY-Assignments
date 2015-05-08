4001c42fd9c5160ca9a013515096da8242528719
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-08 05:49:21 -0400

    Having trouble with node // "chai"

--- string-calculator.js
+++ string-calculator.js
@@ -1,11 +1,11 @@
-var expect = require('chai').expect;
+var expect = require("chai").expect;
 
-function toEnglish(word){
-  if( word === "zero" )
+function toNumber(word){
+  if ( word === "zero" ){
     return 0;
   }
 
-  if( word === "one" ){
+  if ( word === "one" ){
     return 1;
   }
 
@@ -17,21 +17,7 @@ function toEnglish(word){
     return 3;
   }
 
-  if ( word === "four"){
-    return 4;
-  }
 
-  if ( word === "five"){
-    return 5;
-    
-  }
-
-expect(toEnglish("zero")).to.equal(0);
-
-expect(toEnglish("one")).to.equal(1);
-
-expect(toEnglish("two")).to.equal(2);
-
-expect(toEnglish("three")).to.equal(3);
-
-expect(toEnglish("four")).to.equal(4);
+expect(toNumber("zero")).to.equal(0);
+expect(toNumber("one")).to.equal(1);
+expect(toNumber("two")).to.equal(2)
