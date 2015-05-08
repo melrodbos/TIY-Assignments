210d48fb29c27035d0798b8ce7e4c03dd879256a
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-08 06:19:46 -0400

    trying to get this to work!

--- string-calculator.js
+++ string-calculator.js
@@ -5,19 +5,4 @@ function toNumber(word){
     return 0;
   }
 
-  if ( word === "one" ){
-    return 1;
-  }
-
-  if ( word === "two" ){
-    return 2;
-  }
-
-  if ( word === "three"){
-    return 3;
-  }
-
-
-expect(toNumber("zero")).to.equal(0);
-expect(toNumber("one")).to.equal(1);
-expect(toNumber("two")).to.equal(2)
+  expect(toNumber("zero")).to.equal(0);
