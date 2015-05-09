c9b33f4c153fb68735309b6b8a6980588dfd8b48
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-09 17:31:46 -0400

    Addition Failed test

--- string-calculator.js
+++ string-calculator.js
@@ -172,28 +172,3 @@ expect(plus("nine", "eight")).to.equal(17);
 expect(plus("nine", "nine")).to.equal(18);
 
 console.log(plus("zero", "zero"));
-
-
-
-function minus(A,B){
-  return toNumber(A) - toNumber(B);
-}
-
-expect(minus).to.exist;
-expect(minus("one", "zero")).to.equal(1);
-
-
-function multiply(A,B){
-  return toNumber(A) * toNumber(B);
-}
-
-expect(multiply).to.exist;
-expect(multiply("one", "zero")).to.equal(0);
-
-
-function divide(A,B){
-  return toNumber(A) / toNumber(B);
-}
-
-expect(divide).to.exist;
-expect(divide("one", "zero")).to.equal(0);
