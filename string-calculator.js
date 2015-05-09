28f8864b19db0515a10aefb3ab92ce65fc657ca3
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-09 17:52:17 -0400

    Substraction Failed

--- string-calculator.js
+++ string-calculator.js
@@ -1,62 +1,62 @@
 var expect = require("chai").expect;
 
-function toNumber(word){
-  if ( word === "zero" ){
+function toNumber(word) {
+  if (word === "zero") {
     return 0;
   }
 
-  if ( word === "one" ){
+  if (word === "one") {
     return 1;
   }
 
-  if ( word === "two" ){
+  if (word === "two") {
     return 2;
   }
 
-  if ( word === "three" ){
+  if (word === "three") {
     return 3;
   }
 
-  if ( word === "four" ){
+  if (word === "four") {
     return 4;
   }
 
-  if ( word === "five" ){
+  if (word === "five") {
     return 5;
   }
 
-  if ( word === "six" ){
+  if (word === "six") {
     return 6;
   }
 
-  if ( word === "seven" ){
+  if (word === "seven") {
     return 7;
   }
 
-  if ( word === "eight" ){
+  if (word === "eight") {
     return 8;
   }
 
-  if ( word === "nine" ){
+  if (word === "nine") {
     return 9;
   }
 
 }
 
-  expect(toNumber("zero")).to.equal(0);
-  expect(toNumber("one")).to.equal(1);
-  expect(toNumber("two")).to.equal(2);
-  expect(toNumber("three")).to.equal(3);
-  expect(toNumber("four")).to.equal(4);
-  expect(toNumber("five")).to.equal(5);
-  expect(toNumber("six")).to.equal(6);
-  expect(toNumber("seven")).to.equal(7);
-  expect(toNumber("eight")).to.equal(8);
-  expect(toNumber("nine")).to.equal(9);
+expect(toNumber("zero")).to.equal(0);
+expect(toNumber("one")).to.equal(1);
+expect(toNumber("two")).to.equal(2);
+expect(toNumber("three")).to.equal(3);
+expect(toNumber("four")).to.equal(4);
+expect(toNumber("five")).to.equal(5);
+expect(toNumber("six")).to.equal(6);
+expect(toNumber("seven")).to.equal(7);
+expect(toNumber("eight")).to.equal(8);
+expect(toNumber("nine")).to.equal(9);
 
 
-  function plus(A,B){
-    return toNumber(A) + toNumber(B);
+function plus(A, B) {
+  return toNumber(A) + toNumber(B);
 }
 
 expect(plus).to.exist;
@@ -172,3 +172,23 @@ expect(plus("nine", "eight")).to.equal(17);
 expect(plus("nine", "nine")).to.equal(18);
 
 console.log(plus("zero", "zero"));
+console.log(plus("zero", "one"));
+
+function minus(A, B) {
+  return toNumber(A) - toNumber(B);
+}
+
+expect(minus).to.exist;
+expect(minus("zero", "zero")).to.equal(0);
+expect(minus("zero", "one")).to.equal(1);
+expect(minus("zero", "two")).to.equal(2);
+expect(minus("zero", "three")).to.equal(3);
+expect(minus("zero", "four")).to.equal(4);
+expect(minus("zero", "five")).to.equal(5);
+expect(minus("zero", "six")).to.equal(6);
+expect(minus("zero", "seven")).to.equal(7);
+expect(minus("zero", "eight")).to.equal(8);
+expect(minus("zero", "nine")).to.equal(8);
+
+console.log(minus("zero", "zero"));
+console.log(minus("zero", "one"));
