895d3d5572cc9e4c00852f9ae13d534d28ffbe36
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-09 08:46:34 -0400

    testing-madness.js

--- testing-madness.js
+++ testing-madness.js
@@ -7,3 +7,20 @@ function toNumbers(A,B){
 toNumbers(0, 1);
 
 console.log(toNumbers("0", "1"));
+
+
+function toNumbers(A,B){
+  return A - B;
+}
+
+toNumbers(1, 1);
+
+console.log(toNumbers("1", "1"));
+
+function toNumbers(A,B){
+  return A * B;
+}
+
+toNumbers(1, 1);
+
+console.log(toNumbers("1", "1"));
