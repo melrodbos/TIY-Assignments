e72ee383ac796dffd0aae684fb31672e380674de
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-10 14:24:11 -0400

    Testing waters...

--- check-writing.js
+++ check-writing.js
@@ -1,1 +1,23 @@
 /**
+function minus(A,B){
+  return toNumber(A) - toNumber(B);
+}
+
+expect(minus).to.exist;
+expect(minus("one", "zero")).to.equal(1);
+
+
+function multiply(A,B){
+  return toNumber(A) * toNumber(B);
+}
+
+expect(multiply).to.exist;
+expect(multiply("one", "zero")).to.equal(0);
+
+
+function divide(A,B){
+  return toNumber(A) / toNumber(B);
+}
+
+expect(divide).to.exist;
+expect(divide("zero", "zero")).to.equal(0);
