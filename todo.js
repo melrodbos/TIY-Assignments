2faf0b706dbe6b16b3972c4ce4881ab9e0875240
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-11 16:09:17 -0400

    Saving what was in `todo.js`

--- todo.js
+++ todo.js
@@ -32,3 +32,45 @@ expect(taskList.length).to.equal(3);
 addTaskToList("Laundry AM", taskList);
 expect(taskList[3]).to.equal("Laundry AM");
 expect(taskList.length).to.equal(4);
+
+
+
+
+// I can delete a task...
+function addTaskToList(task, list){
+    return list.push({
+      text: task, completed: false
+    });
+    // add task to list somehow in the bottom
+}
+
+expect(taskList.length).to.equal(0);
+addTaskToList("Remember the milk", taskList);
+expect(taskList.length).to.equal(1);
+expect(taskList[0]).text)
+to.equal.
+
+// in order to prioritize we need to delete a task first.
+// Remember: strings are not always the correct answer
+// how about splice??? or slice???
+
+// inputs- outputs- side effects!!!
+//
+// what are the inputs of slice??
+// what are the outputs and the side effects??
+//
+// what are the inputs of splice??
+// what are the outputs and the side effects??
+//
+// what if we leave splice empty??? what happens??
+
+
+//Click on your cheat sheet!!!!
+
+
+// I can edit the task...
+
+
+
+
+// I can prioritize a task...
