0a62e99739bdff65aaabad0090ec7c64d21552b0
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-11 16:13:27 -0400

    more questions added

--- practice-string-calculator.js
+++ practice-string-calculator.js
@@ -1,15 +1,27 @@
 var expect = require("chai").expect;
 
-function toNumbers( A + B ){
-  return  0 + 0;
-}
-
-expect(toNumbers).to.exist;
-expect(toNumbers("A", "B")).to.equal(0);
-
-
+// function toNumbers( A + B ){
+//   return  0 + 0;
+// }
+//
+// expect(toNumbers).to.exist;
+// expect(toNumbers("A", "B")).to.equal(0);
 
 
+function divide(A, B) {
+  if (B === "zero"){
+    return Infinity;
+  }
+  if (A === "one"){
+    if (B === "zero"){
+      return Infinity;
+  }
+
+  return 1;
+
+  }
+
+  return 0; 
 
 
 
