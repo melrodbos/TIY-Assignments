4e6b0856cab60e5b188987df7e7fc282174cc556
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-23 22:34:16 -0400

    Updating copy from class

--- FullWidthTabs/scripts.js
+++ FullWidthTabs/scripts.js
@@ -1,86 +1,13 @@
-//Reference from lecture on NestingAccordeon:
-// document.head.parentElement.className = "js";
+// (function(window){
+//   var BASE_URL = 'http://lorempixel.com/400/200/abstract';
 //
-// var elements = document.querySelectorAll('.cbp-nttrigger');
+//   function rando(limit){
+//     return Math.floor(Math.random() * limit);
+//   }
 //
-// _.forEach(elements, function(element, index, elements){
-//     element.addEventListener('click', function(){
-//         element.parentElement.classList.toggle('cbp-ntopen');
-//    });
-// });
-
-
-
-
-/**
- * cbpFWTabs.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
-;( function( window ) {
-
-	'use strict';
-
-	function extend( a, b ) {
-		for( var key in b ) {
-			if( b.hasOwnProperty( key ) ) {
-				a[key] = b[key];
-			}
-		}
-		return a;
-	}
-
-	function CBPFWTabs( el, options ) {
-		this.el = el;
-		this.options = extend( {}, this.options );
-  		extend( this.options, options );
-  		this._init();
-	}
-
-	CBPFWTabs.prototype.options = {
-		start : 0
-	};
-
-	CBPFWTabs.prototype._init = function() {
-		// tabs elemes
-		this.tabs = [].slice.call( this.el.querySelectorAll( 'nav > ul > li' ) );
-		// content items
-		this.items = [].slice.call( this.el.querySelectorAll( '.content > section' ) );
-		// current index
-		this.current = -1;
-		// show current content item
-		this._show();
-		// init events
-		this._initEvents();
-	};
-
-	CBPFWTabs.prototype._initEvents = function() {
-		var self = this;
-		this.tabs.forEach( function( tab, idx ) {
-			tab.addEventListener( 'click', function( ev ) {
-				ev.preventDefault();
-				self._show( idx );
-			} );
-		} );
-	};
-
-	CBPFWTabs.prototype._show = function( idx ) {
-		if( this.current >= 0 ) {
-			this.tabs[ this.current ].className = '';
-			this.items[ this.current ].className = '';
-		}
-		// change current
-		this.current = idx != undefined ? idx : this.options.start >= 0 && this.options.start < this.items.length ? this.options.start : 0;
-		this.tabs[ this.current ].className = 'tab-current';
-		this.items[ this.current ].className = 'content-current';
-	};
-
-	// add to global namespace
-	window.CBPFWTabs = CBPFWTabs;
-
-})( window );
+//   function randoImage(){
+//     return BASE_URL + '?' + rando(1000);
+//   }
+//
+//   $('img').attr('src', randoImage);
+// })(window);
